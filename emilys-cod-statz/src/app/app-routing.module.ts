19c040e6ed2bad010aff7b9ffe112a6c848c33a6
Emily Potter <emily_p123@hotmail.co.uk>
2020-04-17 22:22:05 +0100

    last 20 games kills graph working

--- emilys-cod-statz/src/app/app-routing.module.ts
+++ emilys-cod-statz/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AverageKillsComponent } from './components/average-kills/average-kills.component';
+import { LastTwentyComponent } from './components/last-twenty/last-twenty.component';
 
 
 const routes: Routes = [
-  {path: '', component: AverageKillsComponent}
+  {path: '', component: LastTwentyComponent}
 ];
 
 @NgModule({
--- emilys-cod-statz/src/app/app.module.ts
+++ emilys-cod-statz/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AverageKillsComponent } from './components/average-kills/average-kills.component';
 import { HttpClientModule } from '@angular/common/http';
+import { LastTwentyComponent } from './components/last-twenty/last-twenty.component';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AverageKillsComponent
+    AverageKillsComponent,
+    LastTwentyComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
