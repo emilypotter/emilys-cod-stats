9a908662b05e112b73d171d6532d719ee8e30eda
Emily Potter <emily_p123@hotmail.co.uk>
2020-04-18 04:28:32 +0100

    bootstrap grid working, average kills coming back from scheduler locally

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 var schedule = require('node-schedule');
 const app = express();
 const users = require('./routes/users');
+const API = require('call-of-duty-api')();
 
 //port number
 const port = 3000;
@@ -23,6 +24,20 @@ app.listen(port, () => {
 });
 
 // setup scheduler
-schedule.scheduleJob('42 * * * * *', function(){
-  console.log('Hi');
+schedule.scheduleJob('05 * * * * *', function(){
+  API.login('emily_p123@hotmail.co.uk', 'N3llytheelephan!').then(() => {
+    API.MWstats('v%20reactionz%20x', API.platforms.xbl).then((output) => {
+      var kills = output.lifetime.mode.br.properties.kills;
+      var games = output.lifetime.mode.br.properties.gamesPlayed;
+      var averageKills = kills / games;
+      // TODO: write average kills to db
+      // deploy to see if it works when server isn't running locally
+      // draw graph from db data
+      console.log(averageKills);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }).catch((err) => {
+    console.log(err);
+  });
 });
--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/stats', (req, res, next) => {
     API.login('emily_p123@hotmail.co.uk', 'N3llytheelephan!').then(() => {
-        API.MWcombatwz('v%20reactionz%20x', API.platforms.xbl).then((output) => {
+        API.MWstats('v%20reactionz%20x', API.platforms.xbl).then((output) => {
             return res.json({succes: true, body: output});
         }).catch((err) => {
             console.log(err);
